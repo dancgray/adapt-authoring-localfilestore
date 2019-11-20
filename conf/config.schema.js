0552e0bd1f8d344750309ea703ad8ae63e560136
Dan Gray <dan@sinensis.co.uk>
2019-11-20 17:35:55 +0000

    Add additional config values

--- conf/config.schema.js
+++ conf/config.schema.js
@@ -4,6 +4,16 @@ module.exports = {
       type: 'String',
       default: '200MB',
       description: 'Maximum file upload size.'
+    },
+    useAppRoot: {
+      type: 'Boolean',
+      default: true,
+      description: 'Use application folder as root directory.'
+    },
+    dataFolder: {
+      type: 'String',
+      default: 'data',
+      description: 'Folder for storing files.'
     }
   }
 };
